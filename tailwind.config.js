59f33119c237d704b98835bb07e6581fb4cbdb4c
ilhanaquib <eanaquib03@gmail.com>
2025-06-27 23:14:22 +0800

    created puzzles

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//     theme: {
+//         extend: {
+//             colors: {
+//                 olive: '#6E7B58', // you can name it whatever you want
+//             },
+//         },
+//     },
+//     content: ['./**/*.html'], // make sure your HTML files are included
+// }
